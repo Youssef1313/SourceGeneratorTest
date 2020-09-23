8db3d0e3346bf1e35e7f59673a25054a762b708e
Youssef Victor <youssefvictor00@gmail.com>
2020-09-23 19:31:01 +0200

    Initial commit

---
+++ MyGenerator/MyGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+namespace MyGenerator
+{
+    [Generator]
+    public class MyGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // Expected to have a compiler error when running dotnet build on the other project.
+            context.AddSource("test.g.cs", SourceText.From("sdfgkljh", Encoding.UTF8));
+        }
+    }
+}
